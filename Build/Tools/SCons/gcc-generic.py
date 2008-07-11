202a8fe41957d9c18557bb1956f0bb2dab265aab
bok <bok@266ca35e-1b0b-0410-907f-cfdd56a08a47>
2008-07-11 07:43:26 +0000

    Better scons build (0.98.5 compatible)

--- Build/Tools/SCons/gcc-generic.py
+++ Build/Tools/SCons/gcc-generic.py
@@ -3,28 +3,28 @@ import os
 def generate(env, gcc_cross_prefix=None, gcc_strict=True, gcc_stop_on_warning=True):
     ### compiler flags
     if gcc_strict:
-        c_compiler_compliance_flags = '-pedantic'
-        c_compiler_warnings         = '-Wall -W -Wundef -Wno-long-long -Wmissing-prototypes -Wmissing-declarations'
+        env.AppendUnique(CCFLAGS = ['-pedantic', '-Wall',  '-W',  '-Wundef', '-Wno-long-long'])
     else:
-        c_compiler_compliance_flags = ''
-        c_compiler_warnings         = '-Wall'
-                
-    c_compiler_defines = '-D_REENTRANT'
+        env.AppendUnique(CCFLAGS = ['-Wall'])
+    
+    compiler_defines = ['-D_REENTRANT']
+    env.AppendUnique(CCFLAGS  = compiler_defines)
+    env.AppendUnique(CPPFLAGS = compiler_defines)
     
     if env['build_config'] == 'Debug':
-        c_compiler_flags = '-g'    
+        env.AppendUnique(CCFLAGS = '-g')
     else:
-        c_compiler_flags = '-O3'
+        env.AppendUnique(CCFLAGS = '-O3')
     
     if gcc_stop_on_warning:
-        c_compiler_flags += ' -Werror'
-
+        env.AppendUnique(CCFLAGS = ['-Werror'])
+        
     if gcc_cross_prefix:
         env['ENV']['PATH'] += os.environ['PATH']
         env['AR']     = gcc_cross_prefix+'-ar'
         env['RANLIB'] = gcc_cross_prefix+'-ranlib'
         env['CC']     = gcc_cross_prefix+'-gcc'
+        env['CXX']    = gcc_cross_prefix+'-g++'
+        env['LINK']   = gcc_cross_prefix+'-g++'
 
-    env['CPPFLAGS'] = ' '.join([c_compiler_defines])
-    env['CCFLAGS']  = ' '.join([c_compiler_compliance_flags, c_compiler_flags, c_compiler_warnings])
     
